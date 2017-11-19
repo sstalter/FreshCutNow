78de5a9682beb0a4ba1b9aee34abe07aa25e8abd
Scott Stalter <scottstalter@live.com>
2017-11-19 15:46:05 -0500

    Update to flowers

---
+++ src/sampleflowers.js
@@ -0,0 +1,36 @@
+// This is just some sample data so you don't have to think of your own!
+let flowers = {
+  flower1: {
+    name: 'Red Roses',
+    image: '',
+    description: 'Everyones favorite red flower. Get a nice dozen.',
+    price: 1724,
+    status: 'available'
+  },
+
+  flower2: {
+    name: 'Yellow Roses',
+    image: '',
+    description: 'Be different.  Get the yellow dozen.',
+    price: 3200,
+    status: 'available'
+  },
+
+  flower3: {
+    name: 'Daisy',
+    image: '',
+    description: 'How do you daisy do or get some daisy dukes',
+    price: 1684,
+    status: 'unavailable'
+  },
+
+  flower4: {
+    name: 'Carnations',
+    image: '',
+    description: 'Carnations are fun.',
+    price: 1129,
+    status: 'available'
+  }
+};
+
+export default flowers;
